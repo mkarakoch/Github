f71018c5f631a3a762656b2b3f167a3239f49c4f
Muhammet <mkarakoc@live.com>
2021-08-01 15:40:18 -0400

    Second commit

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,5 +3,6 @@ package package1;
 public class Class1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Batch 35 36 Github");
     }
 }
