3c50b08514960b2d5ef34bdef56c629269622726
Muhammet <mkarakoc@live.com>
2021-08-01 15:42:15 -0400

    3.rd commit

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -4,5 +4,6 @@ public class Class1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Batch 35 36 Github");
+        System.out.println("I like Git ");
     }
 }
