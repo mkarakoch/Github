134bbb0459402f407ae57ab31fd1855596bf4afe
Muhammet <mkarakoc@live.com>
2021-08-07 15:34:57 -0400

    update

--- out/production/githubdemo/src/SelfStudy/GitPush.java
+++ out/production/githubdemo/src/SelfStudy/GitPush.java
@@ -3,9 +3,12 @@ package SelfStudy;
 public class GitPush {
 
     public static void main(String[] args) {
-        int x=5;
-        int sum = x+2;
+        int x=7;
+        int sum = x+66;
+
 
         System.out.println(sum);
+
+        
     }
 }
