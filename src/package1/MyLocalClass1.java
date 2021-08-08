4137243faebdd7d530a10cb75943b8b48e4e45e0
Muhammet <mkarakoc@live.com>
2021-08-08 14:13:17 -0400

    my local commit 1

---
+++ src/package1/MyLocalClass1.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class MyLocalClass1 {
+    public static void main(String[] args) {
+        // This is my local class 1
+        // I will create a new version and push this code to my remote branch
+
+    }
+}
